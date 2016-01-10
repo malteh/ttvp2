8fcdbf97671cf353642c5913b3ed6f3b92aa47b1
Student424 <lukas.k320@hotmail.de>
2016-01-10 20:56:38 +0100

    Kommentierung
    Kommentierung von Starter.java

--- open-chord_inet/src/de/hawhh/ttv/Starter.java
+++ open-chord_inet/src/de/hawhh/ttv/Starter.java
@@ -6,31 +6,45 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
-
 import de.hawhh.ttv.Strategy.Plan;
 
 public class Starter {
 
+	// Deklaration des Loggings fuer Console und File history.log
 	private static Logger logger = Logger.getLogger(Starter.class);
 
 	public static void main(String[] args) {
+		// Initialisierung des gesamten Spiels
 		logger.info("Starting ...");
 		de.uniba.wiai.lspi.chord.service.PropertiesLoader.loadPropertyFile();
 		// TODO set parameters via gui
+		
+		// GUI Initialisierung  
 		new Ui();
 		//new Starter("141.22.27.30", 8080, 0, true);
 	}
 
 	Map<String, ChordNode> nodes = new HashMap<String, ChordNode>();
 
+	// Initialisierung der GUI Paramter
+    // Server IP - serverHostname
+	// Lokale IP - locAdr
+	// Port - serverPort
+	// Wieviele Gegner - nodesToStart
+	// GUI Checkbox selbstdeklarierter Server - startServer
+	
 	public Starter(String serverHostname, String locAdr, int serverPort, int nodesToStart, Boolean startServer) {
+		// Besonderheiten bei der Ausfuerhrung als Server
 		if (startServer) {
 			logger.info("starting server node");
+			// Serverdeklaration bestehend aus Port und lokaler Adresse
 			ChordNode server = new ChordNode(serverPort, locAdr);
 			nodes.put(server.Id(), server);
 		}
 		logger.info("starting " + nodesToStart + " nodes");
+		// Normale Ausfuehrung
 		for (int i = 1; i <= nodesToStart; i++) {
+			// Normale Nodedeklaration
 			ChordNode c = new ChordNode((serverPort + i),locAdr, serverHostname, serverPort);
 			nodes.put(c.Id(), c);
 		}
@@ -39,6 +53,7 @@ public class Starter {
 	public List<String> getNodeList() {
 		List<String> ret = new ArrayList<String>();
 		for (ChordNode chordNode : nodes.values()) {
+			// Beitreten der jeweiligen Nodes und dessen ID im Chord
 			ret.add(chordNode.Id());
 		}
 
@@ -49,6 +64,7 @@ public class Starter {
 		logger.info("starting Game in");
 
 		try {
+			// 3 Sekunden Intervall zum starten des Spiels
 			for (int i = 3; i > 0; i--) {
 				logger.debug(i);
 				Thread.sleep(1000);
@@ -56,7 +72,7 @@ public class Starter {
 		} catch (InterruptedException e) {
 			logger.fatal(e);
 		}
-		
+		// Speilverhalten der Nodes (Strategie)
 		for (ChordNode node : nodes.values()) {
 			node.startGame(Plan.WEAKEST);
 		}
